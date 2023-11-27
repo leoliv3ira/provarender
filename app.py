e140ccc7d8be30de3747e53d7d6c88e4bba712d7
Leonardo Souza de Oliveira <143464506+leoliv3ira@users.noreply.github.com>
2023-11-27 19:41:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
+# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-
-    with open('provaDevOps.html', 'r') as html_file:
-        html_content = html_file.read()
-
-    with open('styleDevOps.css', 'r') as css_file:
-        css_content = css_file.read()
-
-
-    return render_template('provaDevOp.html', html_content=html_content, css_content=css_content)
+    return render_template('provaDevOps.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
